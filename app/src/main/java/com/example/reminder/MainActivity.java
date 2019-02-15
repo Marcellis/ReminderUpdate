caa02d0b5623f504ab69f5bcca0cb303c95e07b3
Marco Marcellis <m.m.c.m.marcellis@hva.nl>
2019-02-15 16:06:08 +0100

    intents added

--- app/src/main/java/com/example/reminder/MainActivity.java
+++ app/src/main/java/com/example/reminder/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.reminder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,22 +11,30 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.TextUtils;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements RecyclerView.OnItemTouchListener{
 
     //instance variables
     private List<Reminder> mReminders;
     private ReminderAdapter mAdapter;
     private RecyclerView mRecyclerView;
     private EditText mNewReminderText;
+
+    private  GestureDetector mGestureDetector;
+    //Constants used when calling the update activity
+    public static final String EXTRA_REMINDER = "Reminder";
+    public static final int REQUESTCODE = 1234;
+    private int mModifyPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
         mReminders = new ArrayList<>();
 
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -68,9 +86,7 @@ public class MainActivity extends AppCompatActivity {
         /*
 
 Add a touch helper to the RecyclerView to recognize when a user swipes to delete a list entry.
-
 An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
-
 and uses callbacks to signal when a user is performing these actions.
 
 */
@@ -93,25 +109,17 @@ and uses callbacks to signal when a user is performing these actions.
                     @Override
 
                     public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
-
-
                         //Get the index corresponding to the selected position
-
                         int position = (viewHolder.getAdapterPosition());
-
                         mReminders.remove(position);
-
                         mAdapter.notifyItemRemoved(position);
-
                     }
 
                 };
 
-
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
-
         itemTouchHelper.attachToRecyclerView(mRecyclerView);
-
+        mRecyclerView.addOnItemTouchListener(this);
     }
 
     private void updateUI() {
@@ -146,4 +154,44 @@ and uses callbacks to signal when a user is performing these actions.
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onInterceptTouchEvent(@NonNull RecyclerView recyclerView, @NonNull MotionEvent motionEvent) {
+        View child = recyclerView.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+        int mAdapterPosition = recyclerView.getChildAdapterPosition(child);
+        if (child != null && mGestureDetector.onTouchEvent(motionEvent)) {
+//            Toast.makeText(this, mReminders.get(mAdapterPosition).getmReminderText(), Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, UpdateActivity.class);
+            mModifyPosition = mAdapterPosition;
+            intent.putExtra(EXTRA_REMINDER,  mReminders.get(mAdapterPosition));
+            startActivityForResult(intent, REQUESTCODE);
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(@NonNull RecyclerView recyclerView, @NonNull MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean b) {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUESTCODE) {
+            if (resultCode == RESULT_OK) {
+                Reminder updatedReminder = data.getParcelableExtra(MainActivity.EXTRA_REMINDER);
+                // New timestamp: timestamp of update
+                mReminders.set(mModifyPosition, updatedReminder);
+                updateUI();
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/reminder/Reminder.java
+++ app/src/main/java/com/example/reminder/Reminder.java
@@ -1,6 +1,9 @@
 package com.example.reminder;
 
-public class Reminder {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Reminder implements Parcelable {
 
     String mReminderText;
 
@@ -20,4 +23,30 @@ public class Reminder {
     public Reminder(String mReminderText) {
         this.mReminderText = mReminderText;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mReminderText);
+    }
+
+    protected Reminder(Parcel in) {
+        this.mReminderText = in.readString();
+    }
+
+    public static final Parcelable.Creator<Reminder> CREATOR = new Parcelable.Creator<Reminder>() {
+        @Override
+        public Reminder createFromParcel(Parcel source) {
+            return new Reminder(source);
+        }
+
+        @Override
+        public Reminder[] newArray(int size) {
+            return new Reminder[size];
+        }
+    };
 }
