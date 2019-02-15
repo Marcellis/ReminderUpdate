fe7511351ef3ea3765001afec3981da827f06ea6
Marco Marcellis <m.m.c.m.marcellis@hva.nl>
2019-02-15 15:24:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/reminder/Reminder.java
@@ -0,0 +1,23 @@
+package com.example.reminder;
+
+public class Reminder {
+
+    String mReminderText;
+
+    @Override
+    public String toString() {
+        return mReminderText ;
+    }
+
+    public String getmReminderText() {
+        return mReminderText;
+    }
+
+    public void setmReminderText(String mReminderText) {
+        this.mReminderText = mReminderText;
+    }
+
+    public Reminder(String mReminderText) {
+        this.mReminderText = mReminderText;
+    }
+}
